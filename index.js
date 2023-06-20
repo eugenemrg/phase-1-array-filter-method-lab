495d9def1156bda8e028608c58ebdb1856f73735
Eugene Aduogo <eugene.aduogo@student.moringaschool.com>
2023-06-20 10:55:40 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(driversNames, name) {
+    return driversNames.filter(driver => driver.toLowerCase() === name.toLowerCase())
+}
+
+function fuzzyMatch(driversNames, name) {
+    return driversNames.filter(driver => driver.charAt(0) === name.charAt(0))
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name)
+}
